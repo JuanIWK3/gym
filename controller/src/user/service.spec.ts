5e3b707b871bdb09d1aee1b2d6bf48078bc36c90
juaniwk3 <juaniwk3@gmail.com>
2023-11-16 11:46:02 -0300

    update data schema

--- controller/src/user/service.spec.ts
+++ controller/src/user/service.spec.ts
@@ -1,40 +1,40 @@
 import { UserService } from "./service"
 
 describe('User Service', () => {
-  let userService: UserService
+    let userService: UserService
 
-  beforeAll(() => {
-    userService = new UserService()
-  })
+    beforeAll(() => {
+        userService = new UserService()
+    })
 
-  it('should get users', async () => {
-    const result = await userService.getUsers()
+    it('should get users', async () => {
+        const result = await userService.getUsers()
 
-    expect(result.rows).toBeInstanceOf(Array)
-  }, 10000)
+        expect(result.rows).toBeInstanceOf(Array)
+    }, 10000)
 
-  it('should create a user', async () => {
-    await userService.createUser('john')
+    it('should create a user', async () => {
+        await userService.createUser('john')
 
-    const result = await userService.getUserById('john')
+        const result = await userService.getUserById('john')
 
-    expect(result.rowLength).toBe(1)
-  }, 10000)
+        expect(result.rowLength).toBe(1)
+    }, 10000)
 
-  it('should not create a user with the same name', async () => {
-    await expect(userService.createUser('john')).rejects.toThrow("User already exists")
-  }, 10000)
+    it('should not create a user with the same name', async () => {
+        await expect(userService.createUser('john')).rejects.toThrow("User already exists")
+    }, 10000)
 
-  it('should delete a user', async () => {
-    await userService.deleteUser('john')
+    it('should delete a user', async () => {
+        await userService.deleteUser('john')
 
-    const result = await userService.getUserById('john')
+        const result = await userService.getUserById('john')
 
-    expect(result.rowLength).toBe(0)
-  }, 10000)
+        expect(result.rowLength).toBe(0)
+    }, 10000)
 
-  it('should not delete a user that does not exist', async () => {
-    await expect(userService.deleteUser('john')).rejects.toThrow("User does not exist")
-  }, 10000)
+    it('should not delete a user that does not exist', async () => {
+        await expect(userService.deleteUser('john')).rejects.toThrow("User does not exist")
+    }, 10000)
 })
 
