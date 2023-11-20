7bc40419fff222094cde02aa5bf14c6d25615bfc
juaniwk3 <juaniwk3@gmail.com>
2023-11-20 18:04:36 -0300

    Fix MQTT import and formatting issues

--- sensor/src/lib/index.ts
+++ sensor/src/lib/index.ts
@@ -1,18 +1,18 @@
-import type { MqttClient } from "mqtt";
+import type { MqttClient } from 'mqtt';
 
 // place files you want to import through the `$lib` alias in this folder.
 export async function waitForMessage(client: MqttClient, topic: string) {
-  return new Promise((resolve, reject) => {
-    client.subscribeAsync(topic).then(() => {
-      client.on("message", (topic, message) => {
-        console.log(message.toString());
+	return new Promise((resolve, reject) => {
+		client.subscribeAsync(topic).then(() => {
+			client.on('message', (topic, message) => {
+				console.log(message.toString());
 
-        resolve(message.toString());
-      });
-    });
+				resolve(message.toString());
+			});
+		});
 
-    setTimeout(() => {
-      reject("timeout");
-    }, 3000);
-  });
+		setTimeout(() => {
+			reject('timeout');
+		}, 3000);
+	});
 }
--- sensor/src/routes/api/+server.ts
+++ sensor/src/routes/api/+server.ts
@@ -1,19 +1,19 @@
-import { json } from '@sveltejs/kit'
-import mqtt from "mqtt"
+import { json } from '@sveltejs/kit';
+import mqtt from 'mqtt';
 
-import { waitForMessage } from "$lib"
+import { waitForMessage } from '$lib';
 
 export async function POST({ request }) {
-  const { name } = await request.json()
+  const { name } = await request.json();
 
-  const client = await mqtt.connectAsync("mqtt://localhost:1883")
+  const client = await mqtt.connectAsync('mqtt://localhost:1883');
 
-  await client.publishAsync("sensor", name)
+  await client.publishAsync('sensor', name);
 
   try {
-    const res = await waitForMessage(client, "sensor-response")
-    return json({ message: res, name })
+    const res = await waitForMessage(client, 'sensor-response');
+    return json({ message: res, name });
   } catch (error) {
-    return json({ message: error, name })
+    return json({ message: error, name });
   }
 }
